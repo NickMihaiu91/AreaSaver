72b646d250fa11e3eda5b557a48477beefb0e3f2
Nick Mihaiu <nick.mihaiu91@gmail.com>
2016-12-22 10:33:49 +0200

    Add area

--- angular/components/area-list/areaList.js
+++ angular/components/area-list/areaList.js
@@ -1,21 +1,57 @@
 (function () {
     angular.module('areaApp').component('areaList', {
-        templateUrl: './angular/components/area-list/areaList.html',
-        controller: ['areaService', AreaListController],
+        //templateUrl: './angular/components/area-list/areaList.html',
+        template: `<div class="row area-list" ng-repeat="area in $ctrl.areas | orderBy: '-area.createdAt' track by area.id">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="row area-item">
+            <div class="col-md-6 col-sm-6 col-xs-6">
+                <div class="area-name pull-right">
+                    {{area.name}}
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-6">
+                <div class="pull-left">
+                    <button class="btn btn-primary">Edit</button>
+                    <button class="btn btn-danger" ng-click="$ctrl.deleteArea(area.id)">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`,
+        controller: ['$scope', AreaListController],
         bindings: {
-
+            areas: '<',
+            onDeleteAreaClick: '&'
         }
     });
 
-    function AreaListController(areaService) {
+    function AreaListController($scope) {
         var ctrl = this;
 
-        ctrl.$onInit = function () {
-            ctrl.areas = areaService.getAreas();
+        ctrl.deleteArea = function (areaId) {
+            if (typeof ctrl.onDeleteAreaClick === 'function') {
+                ctrl.onDeleteAreaClick({
+                    areaId: areaId
+                });
+            }
         };
 
-        ctrl.deleteArea = function (areaId) {
-            areaService.deleteArea(areaId);
+        ctrl.$onChanges = function (changesObj) {
+            console.log('Changes', changesObj);
         };
+
+        ctrl.$doCheck = function () {
+            console.log('Digesting');
+        }
+
+        $scope.$watch('$ctrl.areas', function(newValue) {
+            console.log('watch', newValue);
+          if (angular.isArray(newValue)) {
+          }
+        });
+
+        // setInterval(function () {
+        //     console.log('AHHAHA', ctrl.areas);
+        // }, 3000)
     }
 })();
--- angular/controllers/mainController.js
+++ angular/controllers/mainController.js
@@ -2,10 +2,25 @@
 
     var areaApp = angular.module('areaApp', ['areaServiceModule']);
 
-    areaApp.controller('MainController', ['$scope', 'areaService', function ($scope, areaService) {
-        this.addArea = function(coordinates){
-            areaService.addArea('test', coordinates);
+    areaApp.controller('MainController', ['$scope', 'areaService', '$timeout', function ($scope, areaService, $timeout) {
+        var ctrl = this;
+
+        ctrl.areas = areaService.getAreas();
+
+        ctrl.addArea = function (coordinates) {
+            areaService.addArea('test' + Math.random().toString(), coordinates);
             console.log('Added', coordinates);
         };
+
+        ctrl.deleteArea = function (areaId) {
+            console.log('ddddelete', areaId);
+            areaService.deleteArea(areaId);
+        };
+
+        // $timeout(function(){
+        //     console.log('ceva', ctrl.data);
+        //     ctrl.data.name ='addadada';
+        //     ctrl.ceva = 'ddd';
+        // }, 5000)
     }]);
 })();
